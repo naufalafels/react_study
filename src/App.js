2937c23c785423b2db209003a96785c448c5d4cb
naufalafles <zamrinaufalcode@gmail.com>
2023-12-10 17:03:33 +1100

    ReactJS Properties Study
    Using components including props to return in App function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h3>Age: {props.age}</h3>
+    </>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person 
+        name='John' 
+        lastName={'Doe'} 
+        age={30}
+      />
+      <Person name='Jane' lastName='Doe' age={28}/>
     </div>
   );
 }
